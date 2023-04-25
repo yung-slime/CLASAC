ba6aa8bffbc1ab1dc9b6a2193bb5121008c8045f
yung-slime <snakescitybigstateindia@gmail.com>
2023-04-25 23:16:11 +0530

    clasAc footmenu

---
+++ script.js
@@ -0,0 +1,13 @@
+const listItems = document.querySelectorAll(".navbar-item")
+
+
+function activate() {
+    listItems.forEach((listItem) => {
+        listItem.classList.remove("active")
+        this.classList.add("active")
+        })
+    }
+
+listItems.forEach((listItem) => {
+    listItem.addEventListener("click", activate)
+})
