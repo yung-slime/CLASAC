dd09102bc308f8038e66375627dbae3d5b1027e8
yung-slime <snakescitybigstateindia@gmail.com>
2023-05-19 20:08:48 +0530

    updated shit

--- scanner.js
+++ scanner.js
@@ -1,4 +1,4 @@
-const messageEl = document.querySelector("#output");
+
 const reader = document.querySelector("#reader");
 
 
@@ -11,12 +11,12 @@ const profile = document.querySelector(".profile")
 function onScanSuccess(decodedText, decodedResult) {
   console.log(`Code matched = ${decodedText}`, decodedResult);
   window.location.href = decodedResult;
-  messageEl.textContent = "success";
+
 }
 
 function onScanFailure(error) {
   console.warn(`Code scan error = ${error}`);
-  messageEl.textContent = "error occured.";
+
 }
 
 
@@ -29,50 +29,21 @@ function onScanFailure(error) {
 
 // code to design ui by ourselves. le ggo.
 camera.addEventListener("click", () => {
-  Html5Qrcode.getCameras().then(cameras => {
-    const rearCameras = cameras.filter(camera => camera.label.toLowerCase().includes('back'));
-    if (rearCameras.length > 0) {
-      const scannerObj = new Html5Qrcode("reader", true);
-      scannerObj.start(rearCameras[0].id, {
-        fps: 30,
-        qrbox: 200,
-        aspectRatio: 0.5625 // 16:9 aspect ratio
-      }, onScanSuccess, onScanFailure);
-    } else {
-      messageEl.textContent = 'No rear camera found';
-    }
-  }).catch(err => {
-    messageEl.textContent = err;
-  });
-})
-
-home.addEventListener("click", () => {
-  Html5Qrcode.stop().then((ignore) => {
-    // QR Code scanning is stopped.
-    messageEl.textContent = ignore
-  }).catch((err) => {
-    // Stop failed, handle it.
-    messageEl.textContent = err
-  });
-})
-
-profile.addEventListener("click", () => {
-  Html5Qrcode.stop().then((ignore) => {
-    // QR Code scanning is stopped.
-    messageEl.textContent = ignore
-  }).catch((err) => {
-    // Stop failed, handle it.
-    messageEl.textContent = err
-  });
-})
-
-download.addEventListener("click", () => {
-  Html5Qrcode.stop().then((ignore) => {
-    // QR Code scanning is stopped.
-    messageEl.textContent = ignore
-  }).catch((err) => {
-    // Stop failed, handle it.
-    messageEl.textContent = err
-  });
+  Html5Qrcode.getCameras().then(
+    cameras => {
+      const rearCameras = cameras.filter(camera => camera.label.toLowerCase().includes('back'));
+      if (rearCameras.length > 0) {
+        const scannerObj = new Html5Qrcode("reader", true);
+        scannerObj.start(rearCameras[0].id, {
+          fps: 30,
+          qrbox: 200,
+           // 16:9 aspect ratio
+        }, onScanSuccess, onScanFailure);
+      } else {
+        messageEl.textContent = 'No rear camera found';
+      }
+    }).catch(err => {
+      messageEl.textContent = err;
+    });
 })
 
