21ac554e7f5a44b703d8b67a327f756196f54771
yung-slime <snakescitybigstateindia@gmail.com>
2023-06-12 20:40:42 +0530

    new commits

--- scanner.js
+++ scanner.js
@@ -1,49 +1,20 @@
+import { BrowserQRCodeReader } from "./node_modules/@zxing/browser";
 
-const reader = document.querySelector("#reader");
+const codeReader = new BrowserQRCodeReader();
 
+const videoInputDevices = await ZXingBrowser.BrowserCodeReader.listVideoInputDevices();
+console.log(`videoInputDevices: ${videoInputDevices}`);
 
-const camera = document.querySelector(".camera");
-const home = document.querySelector(".home")
-const download = document.querySelector(".download")
-const profile = document.querySelector(".profile")
-
-
-function onScanSuccess(decodedText, decodedResult) {
-  console.log(`Code matched = ${decodedText}`, decodedResult);
-  window.location.href = decodedResult;
-
-}
-
-function onScanFailure(error) {
-  console.warn(`Code scan error = ${error}`);
-
-}
+const selectedDeviceId = videoInputDevices[0].deviceId;
 
 
-// let html5QrcodeScanner = new Html5QrcodeScanner(
-//   "reader",
-//   { fps: 30, qrbox: { width: 250, height: 250 } },
-//   /* verbose= */ false);
-// html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+console.log(`Started decode from camera with id ${selectedDeviceId}`);
 
+const previewElem = document.querySelector('#camFeed');
 
-// code to design ui by ourselves. le ggo.
-camera.addEventListener("click", () => {
-  Html5Qrcode.getCameras().then(
-    cameras => {
-      const rearCameras = cameras.filter(camera => camera.label.toLowerCase().includes('back'));
-      if (rearCameras.length > 0) {
-        const scannerObj = new Html5Qrcode("reader", true);
-        scannerObj.start(rearCameras[0].id, {
-          fps: 30,
-          qrbox: 200,
-           // 16:9 aspect ratio
-        }, onScanSuccess, onScanFailure);
-      } else {
-        messageEl.textContent = 'No rear camera found';
-      }
-    }).catch(err => {
-      messageEl.textContent = err;
-    });
-})
+const controls = await codeReader.decodeFromVideoDevice(selectedDeviceId, previewElem, (result, error, controls) => {
+  console.log(`this is the decoded result: ${result}`);
+  console.log(controls);
+});
+
 
