9afafb999b633b596866b86d57b089d225fb818e
yung-slime <snakescitybigstateindia@gmail.com>
2023-06-18 13:13:25 +0530

    files updated 18/06

--- scanner.js
+++ scanner.js
@@ -1,20 +1,60 @@
-import { BrowserQRCodeReader } from "./node_modules/@zxing/browser";
+function onSuccessCallback(result) {
+  console.log(`here's the result: ${result}.`);
+}
 
-const codeReader = new BrowserQRCodeReader();
+function onErrorCallback(error) {
+  console.error(`here's the error: ${error}.`);
+}
+const scanConfig = {
+  fps: 20,
+  qrbox: {
+    width: 250,
+    height: 250,
+  },
+}
 
-const videoInputDevices = await ZXingBrowser.BrowserCodeReader.listVideoInputDevices();
-console.log(`videoInputDevices: ${videoInputDevices}`);
+const scanSetupConfig = {
+  formatsToSupport: [Html5QrcodeSupportedFormats.QR_CODE],
+  useBarcodeDetectorIfSupported: false,
+  verbose: true,
+}
 
-const selectedDeviceId = videoInputDevices[0].deviceId;
+// first lets get camera device Id of the 1st cameraDevice access permissions
+
+async function getCameraId() {
+  try {
+    const cameraDevices = await Html5Qrcode.getCameras();
+    console.log(cameraDevices);
+    // cameraDevices: CameraDevice[];
+
+    if (cameraDevices && cameraDevices.length) {
+      const selectedCameraDeviceId = cameraDevices[0].id;
+      return selectedCameraDeviceId;
+    }
+    else {
+      console.error("no camera device found! occured while tryna access to the camera device.")
+    }
+  }
+  catch (error) {
+    console.log(`error logged in while tryna get access to the camera device.\ndescription: ${error}.`)
+  }
+}
+
+// this is an event handler. invoked when scanBtn is clicked.
+async function startScanner() {
+  try {
+    const scanner = new Html5Qrcode("scanner", scanSetupConfig);
+    const activeCameraDeviceId = await getCameraId();
+    scanner.start(activeCameraDeviceId, scanConfig, onSuccessCallback, onErrorCallback);
+  }
+  catch (error) {
+    console.error(`error occured while starting scanner. error: ${error}.`)
+  }
+}
 
 
-console.log(`Started decode from camera with id ${selectedDeviceId}`);
+const scanBtn = document.querySelector(".camera");
+scanBtn.addEventListener('click', startScanner);
 
-const previewElem = document.querySelector('#camFeed');
-
-const controls = await codeReader.decodeFromVideoDevice(selectedDeviceId, previewElem, (result, error, controls) => {
-  console.log(`this is the decoded result: ${result}`);
-  console.log(controls);
-});
 
 
